a9498995b93a61ee6d82d4b2566303f4929edabd
Nasim Uddin <ficklenasim@outlook.com>
2021-02-04 15:43:07 +0600

    chore(package): added chakra ui

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@chakra-ui/react";
+
 const Login: React.FC = () => {
   return (
     <div>
       <h1>Login page</h1>
+      <Button variant="outline">Hello Chakra</Button>
     </div>
   );
 };
